bdf36e51db7b2856e7fa72131787e40f8c262101
Carlos Eduardo <eduardosantoos573@gmail.com>
2023-01-31 22:05:39 -0300

    update: most test to TDD

--- src/__test__/appointment.spec.ts
+++ src/__test__/appointment.spec.ts
@@ -1,5 +1,6 @@
 import { expect, test } from 'vitest'
 import { Appointment } from '../entities/appointment'
+import { getFutureDate } from '../__test__/utils/get-future-date'
 
 test('create an appointment', () => {
   const startsAt = new Date()
@@ -18,11 +19,8 @@ test('create an appointment', () => {
 })
 
 test('cannot create an appointment with same end date and start date', () => {
-  const startsAt = new Date()
-  const endsAt = new Date()
-  
-  startsAt.setDate(startsAt.getDate() +2)
-  endsAt.setDate(endsAt.getDate() +1)
+  const startsAt = getFutureDate('2022-11-22')
+  const endsAt = getFutureDate('2022-11-22')
 
   expect(() => {
     return  new Appointment({
--- src/__test__/create-appointmet.spec.ts
+++ src/__test__/create-appointmet.spec.ts
@@ -1,20 +1,42 @@
 import { describe, expect, it } from "vitest";
+import { InMemoryAppointmentsRepository } from "../../repositories/in-memory/in-memory-appointments-repository";
 import { Appointment } from "../entities/appointment";
 import { CreateAppointment } from "../use-cases/create-appointment";
+import { getFutureDate } from './utils/get-future-date'
 
 describe('Create Appointment', () => {
   it('should be able to create a new Appointment', () => {
-    const createAppointment = new CreateAppointment()
+    const appointmentsRepository = new InMemoryAppointmentsRepository()
+    const createAppointment = new CreateAppointment(appointmentsRepository)
 
-    const startsAt = new Date()
-    const endsAt = new Date()
-
-    endsAt.setDate(endsAt.getDate() + 1)
+    const startsAt = getFutureDate('2022-11-21')
+    const endsAt = getFutureDate('2022-11-22')
 
     expect(createAppointment.execute({
       customers: 'Jhon', 
       startsAt, 
       endsAt})).resolves.toBeInstanceOf(Appointment)
+
+  })
+
+  it('should not be able to create an Appointment with overlapping dates', async () => {
+    const appointmentsRepository = new InMemoryAppointmentsRepository()
+    const createAppointment = new CreateAppointment(appointmentsRepository)
+
+    const startsAt = getFutureDate('2022-11-10')
+    const endsAt = getFutureDate('2022-11-15')
+
+    await createAppointment.execute({
+      customers: 'Jhon',
+      startsAt, 
+      endsAt
+    })
+
+    expect(createAppointment.execute({
+      customers: 'Jhon', 
+      startsAt: getFutureDate('2022-11-14'), 
+      endsAt: getFutureDate('2022-11-18')
+    })).rejects.toBeInstanceOf(Error)
 
   })
 })
