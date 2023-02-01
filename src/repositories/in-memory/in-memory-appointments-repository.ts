1963d7ffbad98f76e1a46721195f53189fc7667c
Carlos Eduardo <eduardosantoos573@gmail.com>
2023-01-31 22:04:02 -0300

    add: repositories para armazenar variaveis

---
+++ src/repositories/in-memory/in-memory-appointments-repository.ts
@@ -0,0 +1,29 @@
+import { areIntervalsOverlapping } from 'date-fns'
+import { Appointment } from "../../src/entities/appointment";
+import { AppointmentsRepository } from "../appointments-repository";
+
+export class InMemoryAppointmentsRepository implements AppointmentsRepository {
+  public items: Appointment[] = []
+
+  async create(appointment: Appointment): Promise<void> {
+    this.items.push(appointment)
+  }
+
+  async findOverlappingAppointment(startsAt: Date, endsAt: Date): Promise<Appointment | null> {
+    const overlappingAppointment = this.items.find(appointment => {
+      return areIntervalsOverlapping(
+        {start: startsAt, end: endsAt},
+        {start: appointment.startAt, end: appointment.endAt},
+        {inclusive: true}
+      )
+    })
+
+    if(!overlappingAppointment) return null
+
+    return overlappingAppointment
+  }
+
+  async remove(): Promise<void> {
+    this.items.pop()
+  }
+}
